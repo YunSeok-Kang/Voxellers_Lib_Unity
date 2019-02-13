afdf19b59d8b99a1aea0801b8f36b96f6b2e323a
YSKang <ysck73@naver.com>
2019-02-13 23:15:09 +0900

    ENH: VoxDebug, VoxPhysics를 테스트 해볼 수 있는 DebuggingExamples, TargetTracker 데모 씬 추가
    - VoxDebug의 테스트를 위해 DebuggingExamples 씬 추가
    - VoxPhyscis의 테스트를 위해 TargetTracker 씬 추가

---
+++ Assets/Demos/DebuggingExamples/Scripts/DrawCubeOnPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCubeOnPosition : MonoBehaviour
+{
+    [SerializeField] private Vector3 cubePosition;
+    [SerializeField] private Vector3 cubeingRotation;
+    [SerializeField] private Vector3 cubeingScale;
+
+    private void OnDrawGizmos()
+    {
+        Voxellers.VoxDebug.DrawCube(cubePosition, Quaternion.Euler(cubeingRotation), cubeingScale);
+    }
+}
