7889254c3e5a18dd562524aa7aa35d18aa5465d7
YSKang <ysck73@naver.com>
2019-07-22 16:30:11 +0900

    ENH: VoxMath의 원 관련 함수를 이용하여 세 점의 좌표를 받아 원의 중심과 반지름을 구하는 예제 제작

---
+++ Assets/Demos/GetCenterOfCircle/Scripts/GetCenterByCubesPos.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCenterByCubesPos : MonoBehaviour
+{
+    public Transform[] posCubes;
+    public Transform centerCube;
+
+    public float pos1R;
+    public float pos2R;
+    public float pos3R;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 centerOfCircle;
+        float radiusOfCircle;
+
+        if (Voxellers.VoxMath.GetCenterOfCircleByThreePos(out centerOfCircle, out radiusOfCircle, posCubes[0].position, posCubes[1].position, posCubes[2].position) == true)
+        {
+            centerCube.transform.position = centerOfCircle;
+
+            pos1R = Vector2.Distance(posCubes[0].position, centerOfCircle);
+            pos2R = Vector2.Distance(posCubes[1].position, centerOfCircle);
+            pos3R = Vector2.Distance(posCubes[2].position, centerOfCircle);
+        }
+    }
+}
